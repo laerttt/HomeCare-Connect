4dbe9f3c9a9db15c3a3cf495809957ff968e37cd
Kelvin Gjikola <kelvingjikola123@gmail.com>
2024-03-20 19:57:40 +0100

    db

---
+++ src/main/java/dev/Homecare/demo/HomecareConnectApplication.java
@@ -0,0 +1,13 @@
+package dev.Homecare.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomecareConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomecareConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/Homecare/demo/HomecareConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.Homecare.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomecareConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
